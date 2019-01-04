72bdd31e5c4c0e89b65db1ca0c5fd3dac6cce5b2
HardCarroll <phanchan_hc@163.com>
2019-01-04 19:24:37 +0800

    optimation newstagClick()

--- js/common.js
+++ js/common.js
@@ -1,66 +1,119 @@
 // JavaScript Document
+// function newstagClick(param) {
+//   param.object.each(function() {
+//   if ($(this).index() === 0) {
+//     // 上一页处理过程
+//     $(this).click(function() {
+//       if(!$(this).hasClass("disabled")) {
+//         $(this).parent().children().last().removeClass("disabled");
+//         $(this).parent().find(".active").removeClass("active").prev().addClass("active");
+//         if($(this).parent().find(".active").index() === $(this).parent().children().first().index()+1) {
+//           $(this).addClass("disabled");
+//         }
+//         var obj = $.ajax({
+//           url: "/news/refresh_news_list.php?token="+param.token+"&cls="+param.class+"&page="+$(this).parent().find(".active").index(),
+//           context: param.list,
+//           success: function(res) {
+//             // $(this).html(obj.responseText);
+//             $(this).html(res);
+//           }
+//         });
+//       }
+//     });
+//   }
+//   else if ($(this).index() === $(this).parent().children().length-1) {
+//     // 下一页处理过程
+//     $(this).click(function() {
+//       if(!$(this).hasClass("disabled")) {
+//         $(this).parent().children().first().removeClass("disabled");
+//         $(this).parent().find(".active").removeClass("active").next().addClass("active");
+//         if($(this).parent().find(".active").index() === $(this).parent().children().last().index()-1) {
+//           $(this).addClass("disabled");
+//         }
+//         var obj = $.ajax({
+//           url: "/news/refresh_news_list.php?token="+param.token+"&cls="+param.class+"&page="+$(this).parent().find(".active").index(),
+//           context: param.list,
+//           success: function(res) {
+//             // $(this).html(obj.responseText);
+//             $(this).html(res);
+//           }
+//         });
+//       }
+//     });
+//   }
+//   else {
+//     $(this).click(function() {
+//       $(this).addClass("active").siblings().removeClass("active");
+//       var obj = $.ajax({
+//         url: "/news/refresh_news_list.php?token="+param.token+"&cls="+param.class+"&page="+$(this).index(),
+//         context: param.list,
+//         success: function(res) {
+//           // $(this).html(obj.responseText);
+//           $(this).html(res);
+//         }
+//       });
+
+//       $(this).parent().children().first().removeClass("disabled");
+//       $(this).parent().children().last().removeClass("disabled");
+//       if($(this).index() === 1) {
+//         $(this).parent().children().first().addClass("disabled");
+//       }
+//       else if($(this).index() === $(this).parent().children().length-2) {
+//         $(this).parent().children().last().addClass("disabled");
+//       }
+//     });
+//   }
+//   });
+// }
 function newstagClick(param) {
   param.object.each(function() {
-  if ($(this).index() === 0) {
-    // 上一页处理过程
     $(this).click(function() {
-      if(!$(this).hasClass("disabled")) {
-        $(this).parent().children().last().removeClass("disabled");
-        $(this).parent().find(".active").removeClass("active").prev().addClass("active");
-        if($(this).parent().find(".active").index() === $(this).parent().children().first().index()+1) {
-          $(this).addClass("disabled");
-        }
-        var obj = $.ajax({
-          url: "/news/refresh_news_list.php?token="+param.token+"&cls="+param.class+"&page="+$(this).parent().find(".active").index(),
-          context: param.list,
-          success: function() {
-            $(this).html(obj.responseText);
+      var curIndex = 0;
+      if ($(this).index() === 0) {
+        // prev process
+        if(!$(this).hasClass("disabled")) {
+          $(this).parent().children().last().removeClass("disabled");
+          $(this).parent().find(".active").removeClass("active").prev().addClass("active");
+          if($(this).parent().find(".active").index() === $(this).parent().children().first().index()+1) {
+            $(this).addClass("disabled");
           }
-        });
+        }
+        curIndex = $(this).parent().find(".active").index();
       }
-    });
-  }
-  else if ($(this).index() === $(this).parent().children().length-1) {
-    // 下一页处理过程
-    $(this).click(function() {
-      if(!$(this).hasClass("disabled")) {
+      else if($(this).index() === $(this).parent().children().length-1){
+        // next process
+        if(!$(this).hasClass("disabled")) {
+          $(this).parent().children().first().removeClass("disabled");
+          $(this).parent().find(".active").removeClass("active").next().addClass("active");
+          if($(this).parent().find(".active").index() === $(this).parent().children().last().index()-1) {
+            $(this).addClass("disabled");
+          }
+        }
+        curIndex = $(this).parent().find(".active").index();
+      }
+      else {
+        // index process
+        $(this).addClass("active").siblings().removeClass("active");
         $(this).parent().children().first().removeClass("disabled");
-        $(this).parent().find(".active").removeClass("active").next().addClass("active");
-        if($(this).parent().find(".active").index() === $(this).parent().children().last().index()-1) {
-          $(this).addClass("disabled");
+        $(this).parent().children().last().removeClass("disabled");
+        if($(this).index() === 1) {
+          $(this).parent().children().first().addClass("disabled");
         }
-        var obj = $.ajax({
-          url: "/news/refresh_news_list.php?token="+param.token+"&cls="+param.class+"&page="+$(this).parent().find(".active").index(),
-          context: param.list,
-          success: function() {
-            $(this).html(obj.responseText);
-          }
-        });
+        else if($(this).index() === $(this).parent().children().length-2) {
+          $(this).parent().children().last().addClass("disabled");
+        }
+        curIndex = $(this).index();
       }
-    });
-  }
-  else {
-    $(this).click(function() {
-      $(this).addClass("active").siblings().removeClass("active");
-      var obj = $.ajax({
-        url: "/news/refresh_news_list.php?token="+param.token+"&cls="+param.class+"&page="+$(this).index(),
-        context: param.list,
-        success: function() {
-          $(this).html(obj.responseText);
-        }
-      });
 
-      $(this).parent().children().first().removeClass("disabled");
-      $(this).parent().children().last().removeClass("disabled");
-      if($(this).index() === 1) {
-        $(this).parent().children().first().addClass("disabled");
-      }
-      else if($(this).index() === $(this).parent().children().length-2) {
-        $(this).parent().children().last().addClass("disabled");
-      }
-    });
-  }
-  });
+      $.ajax({
+        url: "/news/refresh_news_list.php?token="+param.token+"&cls="+param.class+"&page="+curIndex,
+        context: param.list,
+        success: function(res) {
+          $(this).html(res);
+        }
+      }); // ajax_func
+    }); // click_func
+  }); // each_func
 }
 
 function proCarousel() {
